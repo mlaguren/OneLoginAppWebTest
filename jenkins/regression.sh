b4db88be1f18e9308e958fa69d550e666f1dee3b
Melvin Laguren <melvin@onelogin.com>
2014-02-25 09:01:11 -0800

    Hopefully fixed the jenkins report in sauce labs and removed tag from feature file that can cause it to hang

--- jenkins/regression.sh
+++ jenkins/regression.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -xl
 bundle install --path vendor/bundle
-if [ -z "$4" ]
+
+if [ -z "$1" ]
 then
-  bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" -p staging --format json -o cucumber.json --format junit -o sauce.xml --tags @sauce features/app
+  bundle exec cucumber SUITENAME="Local Test" BROWSER="Internet Explorer" OS="Windows 7" -p staging --format json -o cucumber.json features/app/Login.feature
 else
-  bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" VERSION="$4" -p staging --format json -o cucumber.json --format junit -o sauce.xml --tags @sauce features/app
+  if [ -z "$4" ]
+  then
+    bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" -p staging --format json -o cucumber.json --tags @sauce features/app
+  else
+    bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" VERSION="$4" -p staging --format json -o cucumber.json --tags @sauce features/app
+  fi
 fi
