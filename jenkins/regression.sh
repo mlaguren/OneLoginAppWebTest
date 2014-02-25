c55b019f9c88a2b6aa1355f2f53f0ff2078b9ff3
Melvin Laguren <melvin@onelogin.com>
2014-02-24 16:42:48 -0800

    Updating jenkins script

--- jenkins/regression.sh
+++ jenkins/regression.sh
@@ -2,7 +2,7 @@
 bundle install --path vendor/bundle
 if [ -z "$4" ]
 then
-  bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" --format json -o cucumber.json --format junit -o sauce --tags @sauce features/app
+  bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" -p staging --format json -o cucumber.json --format junit -o sauce --tags @sauce features/app
 else
-  bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" VERSION="$4" --format json -o cucumber.json --format junit -o sauce --tags @sauce features/app
+  bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" VERSION="$4" -p staging --format json -o cucumber.json --format junit -o sauce --tags @sauce features/app
 fi
