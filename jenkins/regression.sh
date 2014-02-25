040e21bbd392132400f78b324d8077ac178062d0
Melvin Laguren <melvin@onelogin.com>
2014-02-25 12:22:21 -0800

    adding formatter

--- jenkins/regression.sh
+++ jenkins/regression.sh
@@ -7,8 +7,8 @@ then
 else
   if [ -z "$4" ]
   then
-    bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" -p staging --format json -o cucumber.json --tags @sauce features/app
+    bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" -p staging --format json -o cucumber.json -f pretty --tags @sauce features/app
   else
-    bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" VERSION="$4" -p staging --format json -o cucumber.json --tags @sauce features/app
+    bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" VERSION="$4" -p staging --format json -o cucumber.json -f pretty --tags @sauce features/app
   fi
 fi
