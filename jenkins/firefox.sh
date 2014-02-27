c891b01ed7e3e4b8c9f0466e0e36f19317efd5e3
Melvin Laguren <melvin@onelogin.com>
2014-02-27 14:09:40 -0800

    Adding debug for jenkins and firefox

--- jenkins/firefox.sh
+++ jenkins/firefox.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -xl
-
+ps -ef | grep X
+echo $DISPLAY
 export DISPLAY=:10
+echo $DISPLAY
 bundle install --path vendor/bundle
 
 bundle exec cucumber -p staging --format json -o cucumber.json --tags @firefox_box
