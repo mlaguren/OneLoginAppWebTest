0161774a196f1239937c4e2d2020358ae561a617
Melvin Laguren <melvin@onelogin.com>
2014-02-27 14:29:32 -0800

    Adding xvfb command

--- jenkins/firefox.sh
+++ jenkins/firefox.sh
@@ -4,5 +4,6 @@ echo $DISPLAY
 export DISPLAY=:10
 echo $DISPLAY
 bundle install --path vendor/bundle
-
+/etc/init.d/xvfb start
 bundle exec cucumber -p staging --format json -o cucumber.json --tags @firefox_box
+/etc/init.d/xvfb stop
