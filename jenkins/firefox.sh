c9a57097c0a853f38eac0fc2419b7a830c0eb5e4
Melvin Laguren <melvin@onelogin.com>
2014-02-27 15:09:39 -0800

    Removing Xvfb as plugin for jenkins is installed

--- jenkins/firefox.sh
+++ jenkins/firefox.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -xl
-Xvfb :1 -screen 0 1280x960x24 &
-echo $DISPLAY
-export DISPLAY=:1
 echo $DISPLAY
 bundle install --path vendor/bundle
 bundle exec cucumber -p staging --format json -o cucumber.json --tags @firefox_box
