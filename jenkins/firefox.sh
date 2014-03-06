23e258f55d737ed2fd48a3a7eac7419175d3d334
Melvin Laguren <melvin@onelogin.com>
2014-03-06 11:24:51 -0800

    Removing ' from script

--- jenkins/firefox.sh
+++ jenkins/firefox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xl
 echo $DISPLAY
 bundle install --path vendor/bundle
-bundle exec cucumber -p staging` --format pretty --format json -o cucumber.json --tags @firefox_box
+bundle exec cucumber -p staging --format pretty --format json -o cucumber.json --tags @firefox_box
