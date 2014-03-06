8e1dfdb3733a5a0a38858fb96ec6932fe1535995
Melvin Laguren <melvin@onelogin.com>
2014-03-06 11:47:47 -0800

    Moving jenkins code to shell script

--- jenkins/firefox.sh
+++ jenkins/firefox.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -xl
 echo $DISPLAY
+pwd
+
+rm -rf scenarios
+find features/**/*.feature -type f -exec sed -i '/\@sauce/d' {} \;
+
 bundle install --path vendor/bundle
+
 bundle exec cucumber -p staging --format pretty --format json -o cucumber.json --tags @firefox_box
