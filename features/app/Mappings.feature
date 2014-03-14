ae033a37fcf0bf236d93e9b238701992e64d598f
Melvin Laguren <melvin@onelogin.com>
2014-03-14 15:20:02 -0700

    Create New Mapping Scenario Finished

--- features/app/Mappings.feature
+++ features/app/Mappings.feature
@@ -3,6 +3,13 @@ Feature:  Mappings
 @firefox_box
 Scenario: User cannot create a blank mapping
 
-Given I am on the Mappings Page
+Given I am on the mappings page
 When I try to create a blank mapping
 Then I cannot create a blank mapping 
+
+Scenario:  User Can Create A Mapping
+Given I am on the mappings page
+When I create a new mapping
+Then the mapping is saved
+
+
--- features/step_definitions/app/Mappings.rb
+++ features/step_definitions/app/Mappings.rb
@@ -1,4 +1,4 @@
-Given(/^I am on the Mappings Page$/) do
+Given(/^I am on the mappings page$/) do
   FromLoginPage = LoginPage.new
   FromLoginPage.login_as($SETUP["admin"]["email"], $SETUP["admin"]["password"])
 
@@ -12,10 +12,17 @@ end
 When(/^I try to create a blank mapping$/) do
   FromNewMappingsPage = NewMappingsPage.new
   FromNewMappingsPage.create_blank_mapping
-  sleep 5
 end
 
 Then(/^I cannot create a blank mapping$/) do
   find(:id, 'flasherror').text.should == "Name can't be blank"
 end
 
+When(/^I create a new mapping$/) do
+  FromNewMappingsPage = NewMappingsPage.new
+  @mapping_name = FromNewMappingsPage.create_new_mapping
+end
+
+Then(/^the mapping is saved$/) do
+  FromMappingsPage.mapping_is_enabled(@mapping_name)
+end
