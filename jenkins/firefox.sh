e9dd2f65e36cc0a10239702f19e8f1c269f60e1f
Melvin Laguren <melvin@laguren.net>
2014-03-17 18:09:37 -0700

    Testing parallel tests in jenkins

--- jenkins/firefox.sh
+++ jenkins/firefox.sh
@@ -12,4 +12,5 @@ find features/**/*.feature -type f -exec sed -i '/\@sauce/d' {} \;
 
 bundle install --path vendor/bundle
 
-bundle exec cucumber -p staging --format pretty --format json -o cucumber.json --tags @firefox_box --tags ~@wip
+#bundle exec cucumber -p staging --format pretty --format json -o cucumber.json --tags @firefox_box --tags ~@wip
+bundle exec parallel_cucumber features/app/ -o "-p staging --format pretty --format json -o cucumber.json --tags @firefox_box --tags ~@wip"
