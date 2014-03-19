170f21ee3dbee7e9555a03af92c4bd10406f25d9
Melvin Laguren <melvin@laguren.net>
2014-03-18 23:29:56 -0700

    Adding cookie helper

--- features/step_definitions/app/LaunchAPISteps.rb
+++ features/step_definitions/app/LaunchAPISteps.rb
@@ -1,11 +1,15 @@
 Given(/^I am on the launch page$/) do
   visit('http://htmlpreview.github.io/?https://github.com/mlaguren/OneLoginAppWebTest/blob/master/launch.html')
+   announce_selenium_cookies(Capybara.current_session.driver.browser)
 end
 
 When(/^I launch the app by the short name$/) do
-  click_link('MeetUp Short Name')
+  click_link('MeetUp App ID')
   FromLoginPage = LoginPage.new
+  p cookies[:session_onelogin.com]
   FromLoginPage.login_as($SETUP["admin"]["email"], $SETUP["admin"]["password"])
+   announce_selenium_cookies(Capybara.current_session.driver.browser)
+  sleep 5
 end
 
 Then(/^I am on my app$/) do
