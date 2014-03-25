dfc14d025dbc853a9448ece780e212fb65992918
Melvin Laguren <melvin@laguren.net>
2014-03-24 17:10:45 -0700

    Switching to production

--- jenkins/regression.sh
+++ jenkins/regression.sh
@@ -3,12 +3,12 @@ bundle install --path vendor/bundle
 
 if [ -z "$1" ]
 then
-  bundle exec cucumber SUITENAME="Local Test" BROWSER="Internet Explorer" OS="Windows 7" -p staging features/app/API.feature:9
+  bundle exec cucumber SUITENAME="Local Test" BROWSER="Internet Explorer" OS="Windows 7" -p production features/app/API.feature:9
 else
   if [ -z "$4" ]
   then
-    bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" -p staging --format json -o cucumber.json -f pretty --tags @sauce features/app
+    bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" -p production --format json -o cucumber.json -f pretty --tags @sauce features/app
   else
-    bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" VERSION="$4" -p staging --format json -o cucumber.json -f pretty --tags @sauce features/app
+    bundle exec cucumber SUITENAME="$1" BROWSER="$2" OS="$3" VERSION="$4" -p production --format json -o cucumber.json -f pretty --tags @sauce features/app
   fi
 fi
