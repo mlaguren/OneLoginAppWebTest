c9c1e1e1bce93a564d5e63d5895df9814f239735
Melvin Laguren <melvin@onelogin.com>
2014-04-01 15:33:26 -0700

    New page object reference

--- features/app/UserPrivileges.feature
+++ features/app/UserPrivileges.feature
@@ -1,6 +1,11 @@
 Feature:  Admin grants privileges to user
 
 Scenario:  New user gets assume privileges
+Given I am on the new user page
+When I create a new user
+And I give the user "Assume Privileges"
+Then the user has assume privileges
+
 
 Scenario:  New user gets manage group
 
---
+++ features/step_definitions/app/UserPrivleges.rb
@@ -0,0 +1,18 @@
+When(/^I create a new user$/) do
+  new_user = User.new
+  new_user.save_to_file(new_user,"new_user.txt")
+
+  FromNewUserPage = NewUserPage.new
+  FromNewUserPage.enter_basic_user(new_user)
+  FromNewUserPage.click_save
+end
+
+When(/^I give the user "(.*?)"$/) do |privilege|
+  FromUserPage = UserPage.new
+  FromUserPage.add_privilege(privilege)  
+  sleep 5
+end
+
+Then(/^the user has assume privileges$/) do
+  pending # express the regexp above with the code you wish you had
+end
