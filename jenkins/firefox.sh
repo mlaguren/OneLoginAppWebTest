48549f244fec0dced3ce0c1ed9d241df34494028
Melvin Laguren <melvin@onelogin.com>
2014-04-28 09:47:03 -0700

    Plaing gems back

--- jenkins/firefox.sh
+++ jenkins/firefox.sh
@@ -10,7 +10,7 @@ rm -rf scenarios
 
 find features/**/*.feature -type f -exec sed -i '/\@sauce/d' {} \;
 
-bundle update --path vendor/bundle
+bundle install --path vendor/bundle
 
 bundle exec cucumber -p production --format pretty --format json -o cucumber.json --tags @firefox_box --tags ~@wip
 #bundle exec parallel_cucumber features/app/ -o "-p staging --format pretty --format json -o cucumber.json --tags @firefox_box --tags ~@wip"
